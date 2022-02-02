a556eeca7e9aa3e1a7e942e1b2ac74d1fea355c8
Marcel Wiemers <77615352+m-wiemers@users.noreply.github.com>
2022-02-02 09:34:38 +0100

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
-import "../styles/globals.css";  
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 import Footer from "../components/Footer";
+import styled from "styled-components";
+
+const ContentWrapper = styled.div`
+  flex: 1;
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <>
-  <Component {...pageProps} />
-  <Footer />
-  </>
+  return (
+    <>
+      <ContentWrapper>
+        <Component {...pageProps} />
+      </ContentWrapper>
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
