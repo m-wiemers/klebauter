60627ec73a6efbcce44975da24b27bf7ad0b8f69
madsen <marcel@klebauter.de>
2022-02-04 23:52:28 +0100

    fix card

--- pages/beruf.tsx
+++ pages/beruf.tsx
@@ -4,7 +4,7 @@ const Business = (): JSX.Element => {
   return (
     <Card
       headline="Beruflich"
-      text="Kurz: Web-Entwicklung. Web-Apps -> React, React-Native, Nextjs, TypeScript, HTML, CSS usw. Ein paar Projekte stelle ich hier kurzzeitig vor"
+      text="Kurz: Web-Entwicklung. Web-Apps -> React, React-Native, Nextjs, TypeScript, HTML, CSS usw."
     />
   );
 };
